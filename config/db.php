af4ca3ff6627fb95ba0e04ff6daa8c0cda94f927
carlosHdz100 <chivacorazon44@gmail.com>
2022-09-29 12:52:16 -0500

    probando ignorar base de datos

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=rentacar',
     'username' => 'root',
-    'password' => 'Jafet058#',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
